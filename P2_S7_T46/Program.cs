32f1738fa39db724badfa354c1db8f860d28a32c
aimartyanov <aimartyanov@gmail.com>
2022-12-12 18:09:54 +0300

    arch

---
+++ P2_S7_T46/Program.cs
@@ -0,0 +1,26 @@
+﻿int m = 3;
+int n = 4;
+void FillArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = new int[m, n];
+FillArr(array);
+PrintArr(array);
